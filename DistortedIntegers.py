1c3c93acef8116779d3189b967ac3d8607a961c5
cb373 <cb373@pc3-037-l.cs.st-andrews.ac.uk>
2020-03-05 13:13:15 +0000

    fixed medium implementation

--- DistortedIntegers.py
+++ DistortedIntegers.py
@@ -14,12 +14,12 @@ class DistortedIntegers:
     #         yield DistortedInt(x, self.n, self.alpha)
     #     #
     # overwrite "print"
-    # def __str__(self):
-    #     for x in self.gen(self):
-    #         print(x)
+    def __str__(self):
+        for x in range(self.n):
+            print(DistortedInt(x, self.n, self.alpha))
 
     def size(self):
-        return self.n - 1
+        return self.n
 
 
 class IteratorOfDistortedIntegers:
@@ -27,7 +27,7 @@ class IteratorOfDistortedIntegers:
     def __init__(self, d):
         self.distortedInteger = d
         self.start = 0
-        self.end = d.size()
+        self.end = d.size() - 1
 
     def __iter__(self):
         return self
@@ -39,14 +39,21 @@ class IteratorOfDistortedIntegers:
         raise StopIteration
 
 def HasDistortedEquationProperty(n,alpha):
-    l = []
+    successes = []
     for x in IteratorOfDistortedIntegers(DistortedIntegers(n,alpha)):
-        for y in IteratorOfDistortedIntegers(DistortedIntegers(n,alpha)):
-            for z in IteratorOfDistortedIntegers(DistortedIntegers(n,alpha)):
+        for z in IteratorOfDistortedIntegers(DistortedIntegers(n,alpha)):
+            # for every x and z there is a unique y for x*y = z
+            l = []
+            # check every y for x and z
+            for y in IteratorOfDistortedIntegers(DistortedIntegers(n,alpha)):
                 if (x*y == z):
+                    # add to list of it working
                     l.append(y)
-    # unique y, so much be length 1
-    return len(l) == 1
+            # add length of list of equality holding
+            successes.append(len(l))
+    # return true if unique, ie. all lists have length 1
+    return all(length == 1 for length in successes)
+
 
 
 def TestHasDistortedEquationProperty():
@@ -58,17 +65,18 @@ def TestHasDistortedEquationProperty():
     return l
 
 
-def calc(distortedInts):
-    resultSet = set()
-    # go through every number of combinations ex. [1,2,3] = permute 2, permute 3
-    for i in range(2, len(distortedInts)+1):
-        # go through every possible combination
-        for k in permutations(distortedInts, i):
-            result = reduce((lambda x, y: x * y), k)
-            # converted to string to avoid duplicate distorted ints
-            resultSet.add(str(result))
-    return resultSet
+# def calc(distortedInts):
+#     resultSet = set()
+#     # go through every number of combinations ex. [1,2,3] = permute 2, permute 3
+#     for i in range(2, len(distortedInts)+1):
+#         # go through every possible combination
+#         for k in permutations(distortedInts, i):
+#             result = reduce((lambda x, y: x * y), k)
+#             # converted to string to avoid duplicate distorted ints
+#             resultSet.add(str(result))
+#     return resultSet
 
+# recursive function to continue down
 def recursiveResultFinder(generators: [DistortedInt], current, results):
     for val in generators:
         res = current * val
@@ -85,23 +93,24 @@ def recursiveResultFinder(generators: [DistortedInt], current, results):
 def spanInit(generators: [DistortedInt]):
     results = set()
     for gen in generators:
-        results.add(str(gen))
         # begin recursion
         recursiveResultFinder(generators, gen, results)
     return results # return list of distortedInts in tree span found
 
 if __name__ == '__main__':
-    a = DistortedInt(4,7,3)
-    b = DistortedInt(5,7,3)
-    c = DistortedInt(6,7,3)
+    a = DistortedInt(1,5,3)
+    b = DistortedInt(2,5,3)
+    # c = DistortedInt(6,7,3)
 
-    vals = [a,b,c]
+    vals = [a,b]
     # test = calc(vals)
     #
     # for i in test:
     #     print(i)
 
     s = spanInit(vals)
+    s = list(s)
+    s.sort()
     for i in s:
         print(i)
 
