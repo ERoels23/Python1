3a9f363917302b5efe62f9e762a73aee3810a020
Eric Roels <ero.360@gmail.com>
2020-03-06 13:40:53 +0000

    Merged Charlie with master

--- Testing.py
+++ Testing.py
@@ -5,6 +5,67 @@ from DistortedIntegers import *
 from EasyDistortedIntegers import *
 
 class TestingDistortedInt(unittest.TestCase):
+    '''
+    DistortedInt Creation
+    '''
+
+    # DistortedInt Instantiation Non-null
+    def test_NonNullDistortedInt(self):
+        print("Testing DistortedInt Creation")
+        x = DistortedInt(2, 5, 3)
+        self.assertIsInstance(x, DistortedInt)
+
+    # DistortedInt Holds Correct Values
+    def test_CorrectArgumentsStored(self):
+        x = DistortedInt(2, 5, 3)
+        self.assertTrue(x.object == 2 and x.n == 5 and x.alpha == 3)
+
+    # Edge Case 1: alpha = 0, n = 1 (Valid)
+    def test_ValidAlphaEdgeCase(self):
+        x = DistortedInt(0, 1, 0)
+        self.assertIsInstance(x, DistortedInt)
+
+    # Edge Case 2: alpha = 1, n = 1 (Invalid)
+    # Note that DistortedInt catches the bubbled exception, doesn't throw it - so we must use ValidateArguments method
+    def test_InvalidAlphaEdgeCase(self):
+        self.assertRaises(InvalidArgumentException, ValidateArguments, 0, 1, 1)
+
+    # Invalid Case with Negatives
+    def test_invalidCaseNegatives(self):
+        self.assertRaises(InvalidArgumentException, ValidateArguments, -10, 10, -3)
+
+    # Invalid Case with String args
+    def test_invalidCaseStrings(self):
+        self.assertRaises(InvalidArgumentException, ValidateArguments, "4", 10, "Hello")
+
+
+    '''
+    DistortedInt Multiplication
+    '''
+
+    # Multiplication using values from specification
+    def test_ValidOperandsFromSpec(self):
+        x = DistortedInt(2, 5, 3)
+        y = DistortedInt(4, 5, 3)
+        self.assertEqual((x * y), DistortedInt(3, 5, 3))
+
+    # Multiplication using other valid values
+    def test_GeneralValidOperands(self):
+        x = DistortedInt(7, 15, 9)
+        y = DistortedInt(13, 15, 9)
+        self.assertEqual((x * y), DistortedInt(4, 15, 9))
+
+    # Invalid Operands throws Exception
+    def test_InvalidOperandExceptionThrown(self):
+        x = DistortedInt(3, 10, 5)
+        y = DistortedInt(13, 17, 6)
+        self.assertRaises(InvalidOperandException, ValidateOperands, x, y)
+
+
+    '''
+    Additional Requirements
+    '''
+    
     # Idempotent Property
     def test_HasDistortedIdempotentProperty(self):
         print("\nTesting Idempotent Property: ")
